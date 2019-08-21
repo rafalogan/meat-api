64ba539791a499b1880644c5f4eca4e5cb8cc8ad
Rafael <www.rafadraw@gmail.com>
2019-08-21 16:34:16 -0300

    Primeiro Exemplo com Restify

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log('main script...')
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+
+
+server.get('/hello', (req, res, next) => {
+    res.json({message: 'Hello World!'})
+    return next()
+});
+
+server.listen(3003, () => console.log('API is running on http://localhost:3003'))
