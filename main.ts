683c0197863fd375f2bb06a44f542a20d1bf5cb7
Rafael <www.rafadraw@gmail.com>
2019-08-21 16:57:18 -0300

    Os Objetos Request, Response e Next

--- main.ts
+++ main.ts
@@ -5,10 +5,27 @@ const server = restify.createServer({
     version: '1.0.0'
 });
 
+server.use(restify.plugins.queryParser());
 
-server.get('/hello', (req, res, next) => {
-    res.json({message: 'Hello World!'})
-    return next()
-});
+server.get('/info', [
+    (req, res, next) => {
+        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+            let error: any = new Error();
+            error.statusCode = 400;
+            return next(false) 
+        }
+        return next();
+    },
+    (req, res, next) => {
+        const browser = req.userAgent();
+        const method = req.method;
+        const url = req.href();
+        const path = req.path();
+        const query = req.query;
+    
+        res.send({ browser, method, url, path, query })
+        return next()
+    }
+]);
 
 server.listen(3003, () => console.log('API is running on http://localhost:3003'))
