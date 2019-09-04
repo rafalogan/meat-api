85b733d275d878d52e530e488d39aa2c2248f9d7
RafaDraw <www.rafadraw@gmail.com>
2019-09-04 17:11:02 -0300

    Outros Métodos e Caso de Excecão

--- src/users/users.test.ts
+++ src/users/users.test.ts
@@ -33,8 +33,8 @@ test('get /users', () => {
 
 test('post /users', () => {
    return request(localUrl)
-       .post('/users').
-       send({
+       .post('/users')
+       .send({
            name: 'User Teste',
            email: 'userteste@teste.com',
            password: '123456',
@@ -47,6 +47,36 @@ test('post /users', () => {
            expect(response.body.password).toBeUndefined();
            expect(response.body.cpf).toBe('131.332.721-28');
        }).catch(fail);
+});
+
+test('get /users/aaaa - not found', () => {
+    return request(localUrl)
+        .get('/users/aaaa')
+        .then(response => {
+            expect(response.status).toBe(404)
+        }).catch(fail);
+});
+
+test('patch /users/:id', () => {
+    return request(localUrl)
+        .post('/users')
+        .send({
+            name: 'Novo User Teste',
+            email: 'novouserteste@teste.com',
+            password: '123456'
+        }).then(response => request(localUrl)
+            .patch(`/users/${response.body._id}`)
+            .send({
+                name: 'User Patch'
+            })
+            .then(response => {
+                expect(response.status).toBe(200);
+                expect(response.body._id).toBeDefined();
+                expect(response.body.name).toBe('User Patch');
+                expect(response.body.email).toBe('novouserteste@teste.com');
+                expect(response.body.password).toBeUndefined();
+            })
+        ).catch(fail);
 });
 
 afterAll(() => server.shutdown());
